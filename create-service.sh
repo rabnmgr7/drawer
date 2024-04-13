f55e0f30d80b1fb3f11dbb6c02793cba8b676fe4
rabnmgr7 <rabnmgr7@gmail.com>
2024-04-13 11:48:17 +0000

    image remove and compose remove part added

--- create-service.sh
+++ create-service.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "Removing services"
+docker compose down || true
+echo "Removing images"
+docker image rm -f rabnmgr7/drawer-flask-app:latest || true
+docker image rm -f rabnmgr7/drawer-mysql-db:latest || true
+echo "Creating docker compose services"
 docker compose up -d
