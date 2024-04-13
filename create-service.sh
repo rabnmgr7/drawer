fd98efdf4399d0817f2006cfc83fcec5f0bff79f
rabnmgr7 <rabnmgr7@gmail.com>
2024-04-13 11:53:35 +0000

    image name corrected

--- create-service.sh
+++ create-service.sh
@@ -2,7 +2,7 @@
 echo "Removing services"
 docker compose down || true
 echo "Removing images"
-docker image rm -f rabnmgr7/drawer-flask-app:latest || true
-docker image rm -f rabnmgr7/drawer-mysql-db:latest || true
+docker image rm -f drawer-flask-app:latest || true
+docker image rm -f drawer-mysql-db:latest || true
 echo "Creating docker compose services"
 docker compose up -d
